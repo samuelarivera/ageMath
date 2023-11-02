0e6af88c917502f7fb0a757836a3a34236ee6739
Samuel Rivera <samuel.rivera@mrjaffesclass.com>
2023-11-02 11:40:50 -0700

    added functions for lower and upper age limits, need to make way to exit program step seven

--- PDA.java
+++ PDA.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 public class PDA
 {
@@ -15,13 +16,33 @@ public class PDA
         
          Scanner scanner = new Scanner(System.in);
          int age = 0;
-        while(true){
+         boolean shouldContinue = true;
+        while(shouldContinue == true){
             System.out.println("How old are you?");
+           try{
             age = scanner.nextInt();
-            System.out.println(age);
+            int LOWER = 0;
+            if(age< LOWER){
+               throw new ArithmeticException(age+"is too young!!");
+            }else{
+                System.out.println("you date as young as" +getYoungerAge(age)+ "and you can date as old as"+ getOlderAge(age));
+            }
+           } catch (InputMismatchException error) {
+               System.out.println("please enter an integer");
+               scanner.next();
+           }
         }    
     }
-
+    public int getYoungerAge(int age){
+            int lower = age;
+            lower = (lower/2) + 7;
+            return lower;
+    }
+    public int getOlderAge(int age){
+        int higher =age;
+        higher = higher - 7;
+        return higher * 2;
+    }
     /**
      * The main method instantiates and runs the program
      */
